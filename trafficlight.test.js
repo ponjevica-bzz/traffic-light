90eb95e43b05d34a83488533afceef036ba2397a
ponjevica-bzz <114403262+ponjevica-bzz@users.noreply.github.com>
2025-11-17 11:46:15 +0100

    Add tests for traffic light functionality

--- trafficlight.test.js
+++ trafficlight.test.js
@@ -1,11 +1,26 @@
 //import module traffic light
-//??
+const app = require("./trafficlight");
 
 //test suite
-//??
+describe('test suite traffic light', () => {
     //case red light
-//??
-//??
-//??
-//??
-//??
+    it(`case red light`, () => { //??
+        expect(app.trafficLight("red"))//??
+            .toBe("DON'T WALK!");//??
+    });
+
+     it(`case orange light`, () => { //??
+        expect(app.trafficLight("orange"))//??
+            .toBe("ATTENTION!");//??
+    });
+
+     it(`case green light`, () => { //??
+        expect(app.trafficLight("green"))//??
+            .toBe("WALK!");//??
+    });
+
+     it(`case undefined light`, () => { //??
+        expect(app.trafficLight("undefined"))//??
+            .toBe(undefined);//??
+    });
+});
